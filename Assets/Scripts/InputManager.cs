b20d9f2b54bc6693dbd46deb4fecf935bd6fb6ab
Roman Doliba <romadoliba@gmail.com>
2024-04-04 12:44:13 +0300

    Complete movement scripts

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,21 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    [SerializeField] protected float _jumpPower;
-    [SerializeField] protected float _moveSpeed;
-    [SerializeField] protected Rigidbody2D _playerRigidbody;
-    protected bool _isGrounded;
-    protected bool _isJump;
-    public void Jump()
+    private float _direction;
+    private bool _isJump; 
+    private void Update()
     {
         _isJump = Input.GetKeyDown(KeyCode.W);
-        
-        if (_isJump && _isGrounded)
-        {
-            _playerRigidbody.AddForce(Vector2.up * _jumpPower, ForceMode2D.Impulse);
-        }
+        _direction = Input.GetAxis("Horizontal");
     }
+    public float MoveInput { get { return _direction; } }
+    public bool JumpInput { get { return _isJump; } }
+
 }
